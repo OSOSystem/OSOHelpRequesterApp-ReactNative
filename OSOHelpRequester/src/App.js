d71d0faf3890785b826bbdbf752c8a90e9d4c935
Pierre Wilken <pierre_w@live.com>
2018-10-20 13:27:36 +0200

    ➕redux basic boilerplate

--- OSOHelpRequester/src/App.js
+++ OSOHelpRequester/src/App.js
@@ -3,9 +3,15 @@ import { View, Alert, Linking } from 'react-native';
 import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 import LoggedinForm from './components/LoggedinForm';
+
 import Login from './keycloak/index';
 import { TokenStorage } from './keycloak/TokenStorage';
+
 import Router from './Router';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 
 type Props = {};
@@ -37,7 +43,9 @@ export default class App extends Component<Props> {
 
     render() {
         return (
-            <Router />
+            <Provider store={createStore(reducers)}>
+                <Router />
+            </Provider>
         );
     }
 }
